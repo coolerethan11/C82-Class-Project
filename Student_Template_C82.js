2262bfb26d8b05df573ae8d97637882464f96bd1
coolerethan11 <138809568+coolerethan11@users.noreply.github.com>
2023-09-07 18:46:41 +0100

    Add files via upload

---
+++ Student_Template_C82.js
@@ -0,0 +1,26 @@
+console.log("My first console project");
+
+            var first_name = "Ranbir";
+            var last_name = "Kapoor";
+
+            "first_name.concat(last_name)"
+            var full_name =  Ethan Deluca;
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+var = a + b
+
+            var add = console.log(add)  ;
+            console.log(add);
+
+var sub = a - b
+            var sub =   ;
+            console.log(sub);
+
+var multiple = a * b
+            var multiple =   ;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
